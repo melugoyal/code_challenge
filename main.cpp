5f2642ec01787f34d44f20243461ae2c475731da
melugoyal <mgoyal3@illinois.edu>
2013-09-06 15:24:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,11 +30,12 @@ int main()
 
 bool prime(int a)
 {
-    if (a==1) return false;
-    if (a==0) return false;
+    if (a<=1) 
+        return false;
     for (int i = 2; i < a; i++)
     {
-        if (a%i == 0) return false;
+        if (a%i == 0) 
+            return false;
     }
     return true;
 }
